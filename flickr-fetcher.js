474b7dd82ad6c6143ff2ec9d2251b997eac14d34
German Arduino <garduino@gmail.com>
2017-06-24 23:27:42 -0300

    Intermediate Saving

---
+++ flickr-fetcher.js
@@ -0,0 +1,10 @@
+// flickr-fetcher.js
+var FlickrFetcher;
+
+FlickrFetcher = {
+    photoObjToURL: function() {
+        return 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg';
+    }
+};
+
+module.exports = FlickrFetcher;
