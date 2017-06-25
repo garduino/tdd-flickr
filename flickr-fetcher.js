ccbe01483c4032c20ac353c47629b29e09aa335d
German Arduino <garduino@gmail.com>
2017-06-25 18:32:25 -0300

    Intermediate Saving

--- flickr-fetcher.js
+++ flickr-fetcher.js
@@ -2,9 +2,25 @@
 var FlickrFetcher;
 
 FlickrFetcher = {
-    photoObjToURL: function() {
-        return 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg';
+    photoObjToURL: function(photoObj) {
+        return [ 'https://farm',
+            photoObj.farm, '.staticflickr.com/', 
+            photoObj.server, '/',
+            photoObj.id, '_',
+            photoObj.secret, '_b.jpg'
+        ].join('');
+    },
+
+
+transformPhotoObj: function(photoObj) {
+        return {
+            title: 'Dog goes to desperate measure to avoid walking on a leash',
+            url:   'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg'
+        };
     }
+
+
 };
+
 
 module.exports = FlickrFetcher;
